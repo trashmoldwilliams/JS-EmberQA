a531a0b922d6c840c7cefc041ee4e116e60befd4
Will Johnson <trashmoldwilliams@gmail.com>
2016-04-01 07:58:50 -0700

    Add test values to database and ouytput them onto homepage.

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'qa-board',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://qa-form.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
